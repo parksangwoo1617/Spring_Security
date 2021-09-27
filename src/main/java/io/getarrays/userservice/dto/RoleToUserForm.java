f7889a1fe1063fe5dcf4afd8ec4318f9a08574fd
parksangwoo <b01056721617@gmail.com>
2021-09-27 19:35:03 +0900

    ⚡ :: Role + User DTO

--- src/main/java/io/getarrays/userservice/dto/RoleToUserForm.java
+++ src/main/java/io/getarrays/userservice/dto/RoleToUserForm.java
@@ -1,2 +1,9 @@
-package io.getarrays.userservice.dto;public class RoleToUserForm {
+package io.getarrays.userservice.dto;
+
+import lombok.Data;
+
+@Data
+public class RoleToUserForm {
+    private String username;
+    private String rolename;
 }
